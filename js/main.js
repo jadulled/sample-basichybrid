c6d7f3f9235bb97075a5abdd2e1d9c330210bcf7
eashtian <elroy.ashtian@intel.com>
2013-10-11 11:33:47 -0700

    app designer compliant

---
+++ js/main.js
@@ -0,0 +1,42 @@
+/* Use beep() method of notification object */
+
+    function beepOnce()
+    {
+        try
+        {
+            intel.xdk.notification.beep(1);
+        }
+        catch(e) {}
+    }
+   
+      
+    /* vibrate the device with vibrate() method of notification object */
+    function vibrateDevice()
+    {
+        try
+        {
+            intel.xdk.notification.vibrate();
+        }
+        catch(e) {} 
+    }
+
+    /* play a sound using player object */
+    function playSound()
+    {
+        try
+        {
+            intel.xdk.player.playSound("sounds/bark02.wav"); 
+        }
+        catch(e) { } 
+    }
+    
+    /*Darken the screen*/
+    function playDead()
+    {
+        document.getElementById("shroud").style.display="block";
+    }
+    
+    function wakeUp()
+    {
+        document.getElementById("shroud").style.display="none";
+    }   
